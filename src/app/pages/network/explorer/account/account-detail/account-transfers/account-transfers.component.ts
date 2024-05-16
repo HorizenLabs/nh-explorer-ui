be6bbd271ba9bd4554b80a43ee6f2e33b8c65991
Santi <santi.gamo@gmail.com>
2024-05-16 14:06:51 +0200

    fix: update regex to take the whole value and not only digits and parse with BigNumber lib the scientific notation number

--- src/app/pages/network/explorer/account/account-detail/account-transfers/account-transfers.component.ts
+++ src/app/pages/network/explorer/account/account-detail/account-transfers/account-transfers.component.ts
@@ -26,6 +26,7 @@ import { decodeAddress } from "@polkadot/util-crypto";
 import { NetworkService } from "../../../../../../services/network.service";
 import { TooltipsService } from "../../../../../../services/tooltips.service";
 import { map, shareReplay, switchMap, tap } from 'rxjs/operators';
+import BigNumber from "bignumber.js";
 
 
 type eventAmounts = [string, BN][];
@@ -212,7 +213,8 @@ export class AccountTransfersComponent implements OnChanges, OnDestroy {
     // First check if there is a Transfer available.
     if ((eventOrTransfer as pst.Transfer).hasOwnProperty('amount')) {
       const amounts: eventAmounts = [];
-      amounts.push(['amount', new BN((eventOrTransfer as pst.Transfer).amount)]);
+      const value = BigNumber((eventOrTransfer as pst.Transfer).amount).toFixed(0);
+      amounts.push(['amount', new BN(value)]);
       const observable = of(amounts);
       this.amountsCache.set(key, observable);
       return observable;
@@ -226,15 +228,17 @@ export class AccountTransfersComponent implements OnChanges, OnDestroy {
 
       if (typeof attributes === 'string') {
         for (let name of attrNames) {
-          const match = new RegExp(`"${name}": ?\"?(\\d+)\"?`).exec(attributes);
+          const match = attributes.match(new RegExp(`"${name}": ?"?([+-]?(?:\\d+(?:\\.\\d*)?|\\.\\d+)(?:[eE][+-]?\\d+)?)`));
           if (match) {
-            amounts.push([name, new BN(match[1])]);
+            const value = new BigNumber(match[1]).toFixed(0);
+            amounts.push([name, new BN(value)]);
           }
         }
       } else if (Object.prototype.toString.call(attributes) == '[object Object]') {
         attrNames.forEach((name) => {
           if (attributes.hasOwnProperty(name)) {
-            amounts.push([name, new BN(attributes[name])])
+            const value = new BigNumber(attributes[name]).toFixed(0);
+            amounts.push([name, new BN(value)])
           }
         })
       }
@@ -254,7 +258,8 @@ export class AccountTransfersComponent implements OnChanges, OnDestroy {
       map((event) => {
         const amounts: [string, BN][] = [];
         if (event.attributes) {
-          amounts.push(['amount', new BN(event.attributes[2])]);
+          const value = new BigNumber(event.attributes[2]).toFixed(0);
+          amounts.push(['amount', new BN(value)]);
         }
         return amounts;
       }),
--- src/app/pages/network/explorer/transfer/transfer-list/transfer-list.component.ts
+++ src/app/pages/network/explorer/transfer/transfer-list/transfer-list.component.ts
@@ -28,6 +28,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { BN, u8aToHex } from "@polkadot/util";
 import { decodeAddress } from "@polkadot/util-crypto";
 import { catchError, Observable, of, switchMap } from 'rxjs';
+import BigNumber from "bignumber.js";
 
 
 type eventAmounts = [string, BN][];
@@ -288,7 +289,8 @@ export class TransferListComponent extends PaginatedListComponentBase<pst.Event 
     // First check if there is a Transfer available.
     if ((eventOrTransfer as pst.Transfer).hasOwnProperty('amount')) {
       const amounts: eventAmounts = [];
-      amounts.push(['amount', new BN((eventOrTransfer as pst.Transfer).amount)]);
+      const value = new BigNumber((eventOrTransfer as pst.Transfer).amount).toFixed(0);
+      amounts.push(['amount', new BN(value)]);
       const observable = of(amounts);
       this.amountsCache.set(key, observable);
       return observable;
@@ -302,15 +304,17 @@ export class TransferListComponent extends PaginatedListComponentBase<pst.Event 
 
       if (typeof attributes === 'string') {
         for (let name of attrNames) {
-          const match = new RegExp(`"${name}": ?\"?(\\d+)\"?`).exec(attributes);
+          const match = attributes.match(new RegExp(`"${name}": ?"?([+-]?(?:\\d+(?:\\.\\d*)?|\\.\\d+)(?:[eE][+-]?\\d+)?)`));
           if (match) {
-            amounts.push([name, new BN(match[1])]);
+            const value = new BigNumber(match[1]).toFixed(0);
+            amounts.push([name, new BN(value)]);
           }
         }
       } else if (Object.prototype.toString.call(attributes) == '[object Object]') {
         attrNames.forEach((name) => {
           if (attributes.hasOwnProperty(name)) {
-            amounts.push([name, new BN(attributes[name])])
+            const value = new BigNumber(attributes[name]).toFixed(0);
+            amounts.push([name, new BN(value)])
           }
         })
       }
@@ -330,7 +334,8 @@ export class TransferListComponent extends PaginatedListComponentBase<pst.Event 
       map((event) => {
         const amounts: [string, BN][] = [];
         if (event.attributes) {
-          amounts.push(['amount', new BN(event.attributes[2])]);
+          const value = new BigNumber(event.attributes[2]).toFixed(0);
+          amounts.push(['amount', new BN(value)]);
         }
         return amounts;
       }),
